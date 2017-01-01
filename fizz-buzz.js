6287a5276e8cf919fe8c93fc5b1240fffd05c2e0
Amy Carney <digilou@users.noreply.github.com>
2017-01-01 07:49:26 -0900

    FizzBuzz challenge

---
+++ fizz-buzz.js
@@ -0,0 +1,10 @@
+for (var num = 1; num <= 100; num++){
+  var result = "";
+  if (num % 3 == 0){
+    result += "Fizz";
+  }
+  if (num % 5 == 0){
+    result += "Buzz";
+  }
+  console.log(result || num);
+}
